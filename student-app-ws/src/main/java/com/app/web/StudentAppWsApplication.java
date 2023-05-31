a2e89ae03d9369951ad1994fe15d6ff668dd4e14
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2023-05-31 17:00:33 -0600

    first commit

---
+++ student-app-ws/src/main/java/com/app/web/StudentAppWsApplication.java
@@ -0,0 +1,13 @@
+package com.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAppWsApplication.class, args);
+	}
+
+}
